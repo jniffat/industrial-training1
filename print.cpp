210945898d6f25aad1fb3e1d82a6cd5fe9be2185
Iffat <jniffat99@gmail.com>
2025-01-29 00:20:18 +0600

    Fibonacci series print

--- print.cpp
+++ print.cpp
@@ -2,10 +2,18 @@
 using namespace std;
 int main()
 {
-    int a;
-    for(int a=1;a<=10;a++)
-    {
- cout<<a<<endl;
-    }
-   return 0;
+ int n1=0,n2=1,n3,num;
+ cout<<"Enter how many terms u want : ";
+ cin>>num;
+ cout<<"Fibonacci Seiers: "<<endl;
+ for(int i=0;i<num;i++)
+ {
+     cout<<n1<<endl;
+     n3=n1+n2;
+     n1=n2;
+     n2=n3;
+     
+     
+ }
+ return 0;
 }
